8047f9ef1937e07a1f68af6f96f2af9f2a681bd9
kohei sano <nlee9337@gmail.com>
2025-05-04 22:54:22 +0900

    Implement MessageAuthCode class with HMAC-SHA-256 initialization

--- message_auth_code.py
+++ message_auth_code.py
@@ -1,8 +1,17 @@
-
+import os
 
 class MessageAuthCode:
+    """
+    HMAC-SHA-256
+    """
     def __init__(self):
-        pass
+        self.block_size = 64
+        # Ideally, a Diffie-Hellman key exchange should be used,
+        # but in this implementation, 
+        # the key is generated manually for simplicity.
+        self.key = os.urandom(self.block_size)
+        self.ipad = bytes([0x36] * self.block_size)
+        self.opad = bytes([0x5c] * self.block_size)
 
     def execute(self, message: str) -> str:
         return "test: " + message
