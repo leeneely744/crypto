eebf1453e9c8b97125e7cc666f802c9000297687
kohei sano <nlee9337@gmail.com>
2025-05-05 11:16:36 +0900

    Refactor execute method in MessageAuthCode class for clarity and correctness

--- message_auth_code.py
+++ message_auth_code.py
@@ -15,14 +15,29 @@ class MessageAuthCode:
         self.opad = bytes([0x5c] * self.block_size)
 
     def execute(self, message: str) -> str:
+        # K XOR ipad, text
         k_xor_ipad = bytes([a ^ b for a, b in zip(self.key, self.ipad)])
-        message = k_xor_ipad + message.encode('utf-8')
+        inner_message = k_xor_ipad + message.encode('utf-8')
+
+        # H(K XOR ipad, text)
+        inner_hash = hashlib.sha256(inner_message).digest()
+
+        # K XOR opad
+        k_xor_opad = bytes([a ^ b for a, b in zip(self.key, self.opad)])
+        # K XOR opad, H(K XOR ipad, text)
+        outer_message = k_xor_opad + inner_hash
+        # H(K XOR opad, H(K XOR ipad, text))
+        outer_hash = hashlib.sha256(outer_message).hexdigest()
+        return outer_hash
+
 
 def main():
+    print("This is a HMAC-SHA-256 example.")
+    print("Please enter a message:", end=" ")
+    message = input()
     mac = MessageAuthCode()
-    message = "Hello, World!"
     mac_value = mac.execute(message)
-    print(f"Message: {mac_value}")
+    print(f"MAC: {mac_value}")
 
 if __name__ == "__main__":
     main()
