067d55bcba42784289fe357ed6681d6732ebb18f
kohei sano <nlee9337@gmail.com>
2025-05-06 21:38:38 +0900

    Add encryption and decryption methods to SymmetricKeyCryptography class

--- symmetric-key-cryptography.py
+++ symmetric-key-cryptography.py
@@ -8,6 +8,36 @@ class SymmetricKeyCryptography:
         self.key_size = 128  # bits
         self.round_num = 10  # For 128-bit key size
         self.block_size = 128
+    
+    def encrypto(self, round_index: int):
+        for i in range(round_index):
+            self.sub_bytes()
+            self.shift_rows()
+            self.mix_columns()
+            self.add_round_key()
+    
+    def decrypto(self, round_index: int):
+        for i in range(round_index):
+            self.add_round_key()
+            self.inv_mix_columns()
+            self.inv_shift_rows()
+            self.inv_sub_bytes()
+
+    def sub_bytes(self):
+        # Perform the SubBytes step
+        pass
+
+    def shift_rows(self):
+        # Perform the ShiftRows step
+        pass
+
+    def mix_columns(self):
+        # Perform the MixColumns step
+        pass
+
+    def add_round_key(self):
+        # Perform the AddRoundKey step
+        pass
 
     def execute(self):
         pass
