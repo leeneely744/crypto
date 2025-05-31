863bbc8c22d435d9d328d0ec49fb4eaf4ea65997
kohei sano <nlee9337@gmail.com>
2025-05-31 22:09:25 +0900

    Initialize parameters for ECDSA public-key cryptography in PublicKeyCryptography class

--- public_key_cryptography.py
+++ public_key_cryptography.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 class PublicKeyCryptography:
+    """ECDSA public-key cryptography example."""
     def __init__(self, key: str):
-        self.key = key
+        # p = 2^256 - 2^32 - 977
+        self.p = 115792089237316195423570985008687907852837564279074904382605163141518161494337
+        self.a = 0
+        self.b = 7
+        # G = (x, y) is the generator point
+        self.x = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+        self.y = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+        self.n = 115792089237316195423570985008687907852837564279074904382605163141518161494337
 
     def encrypt(self, plaintext: str) -> str:
         pass
